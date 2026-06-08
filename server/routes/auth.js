c0a6ea402e03e720f214cf1a5b1c530f3d1bf53b
Khumoyun Djuraev <khumoyunv@gmail.com>
2026-06-08 11:48:32 +0200

    done with forget and reset password

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const { registerMember, loginMember } = require('../controllers/authController');
+const { forgotPassword, resetPassword } = require('../controllers/passwordController');
 
 router.post('/register', registerMember);
 router.post('/login', loginMember);
+router.post('/forgot-password', forgotPassword);
+router.post('/reset-password', resetPassword);
 
 module.exports = router;
